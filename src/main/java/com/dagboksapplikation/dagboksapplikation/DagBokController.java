0e6389e8b67b12eb5abe961547f8b55d028aba63
Qutaeba89 <qutaebadandashi@gmail.com>
2024-12-17 16:55:25 +0100

    added edit function

--- src/main/java/com/dagboksapplikation/dagboksapplikation/DagBokController.java
+++ src/main/java/com/dagboksapplikation/dagboksapplikation/DagBokController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,6 +29,17 @@ public class DagBokController {
 
     @PostMapping("/save")
     public String saveDagbok(@ModelAttribute DagBok dagBok) {
+        dagBokRepositry.save(dagBok);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editDagBok(@PathVariable int id, Model model) {
+        model.addAttribute("dagBok", dagBokRepositry.findById(id).orElse(null));
+        return "edit-dagbok";
+    }
+    @PostMapping("/update")
+    public String updataDagbok(@ModelAttribute DagBok dagBok) {
         dagBokRepositry.save(dagBok);
         return "redirect:/";
     }
