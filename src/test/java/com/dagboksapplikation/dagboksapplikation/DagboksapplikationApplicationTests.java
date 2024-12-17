7b611a19952a6331cd7bb9b8f5b050e6b4a7c073
Qutaeba89 <qutaebadandashi@gmail.com>
2024-12-17 08:58:48 +0100

    Created a file

---
+++ src/test/java/com/dagboksapplikation/dagboksapplikation/DagboksapplikationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dagboksapplikation.dagboksapplikation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagboksapplikationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
