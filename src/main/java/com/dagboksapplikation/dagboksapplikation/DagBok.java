b9fac3ce26d29f3defd4668466b21d9d904f7f60
Qutaeba89 <qutaebadandashi@gmail.com>
2024-12-17 16:02:34 +0100

    created DagBok class

---
+++ src/main/java/com/dagboksapplikation/dagboksapplikation/DagBok.java
@@ -0,0 +1,46 @@
+package com.dagboksapplikation.dagboksapplikation;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class DagBok {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+
+    private int id;
+    private String rubrik;
+    private String text;
+    private LocalDate datum;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getRubrik() {
+        return rubrik;
+    }
+    public void setRubrik(String rubrik) {
+        this.rubrik = rubrik;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public LocalDate getDatum() {
+        return datum;
+    }
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
+    
+}
