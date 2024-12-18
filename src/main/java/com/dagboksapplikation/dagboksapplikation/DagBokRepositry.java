7d510e0f81e1e24404d96281ad0bd612307bc225
Qutaeba89 <qutaebadandashi@gmail.com>
2024-12-18 10:53:00 +0100

    fix bug which displya future dagbok now it will display when date is reached

--- src/main/java/com/dagboksapplikation/dagboksapplikation/DagBokRepositry.java
+++ src/main/java/com/dagboksapplikation/dagboksapplikation/DagBokRepositry.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DagBokRepositry extends CrudRepository<DagBok, Integer>{
-    @Query("SELECT d FROM DagBok d WHERE d.deleted=0")
+    @Query("SELECT d FROM DagBok d WHERE d.deleted=0 AND d.datum <=CURRENT_DATE")
     List<DagBok> findNotDeleted();
 }
